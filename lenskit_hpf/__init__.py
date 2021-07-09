ab708f8cc66f1bcb5674faa34bc2573fbc650c7a
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-07-09 15:30:59 -0600

    remove

--- lenskit_hpf/__init__.py
+++ lenskit_hpf/__init__.py
@@ -6,5 +6,5 @@ from . import _pt
 __version__ = '0.14.0'
 __version__ = _pt.gitify_version(__version__)
 
-# if __name__ == 'lenskit_hpf':
-    # from .hpf import HPF  # noqa: F401
+if __name__ == 'lenskit_hpf':
+    from .hpf import HPF  # noqa: F401
